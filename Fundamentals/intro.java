514164a7293eb75f8c664a9beecdd811951ad17b
ShruutiMishra <shrutisaurav04@gmail.com>
2023-10-25 23:13:09 +0530

    commit 2

---
+++ Fundamentals/intro.java
@@ -0,0 +1,21 @@
+//every line of the java code should be inside a class
+public class intro {
+    public static void main(String[] args) {
+        // any code inside main() method is executed
+        System.out.println("hello world");
+
+        // s.o.p is just to priny the output
+
+        // System is a built - in java class which contains useful members
+        // like out and println()
+        // inside the println(), we must use double quotes
+
+        System.out.print("my name is shruti");
+        System.out.println("this is my name ");
+
+        // difference between print and println is that..
+        // in print(), it does not add a new line at the end of output
+        // println(), it creates a new line to print the output
+
+    }
+}
---
+++ Fundamentals/variables.java
@@ -0,0 +1,34 @@
+package Fundamentals;
+
+//variables are containers for storing data values
+
+// String,
+//int 
+//float
+//char
+//boolean
+
+public class variables {
+    public static void main(String[] args) {
+        /*
+         * Declare variables syntax
+         * type variable_name = value;
+         */
+        String name = "Shruti";
+        System.out.println("name is " + name);
+        int age = 19;
+        System.out.println("age is " + age);
+        boolean mybool = true;
+        char letter = 'D';
+        float mark = 90.2f;
+
+        /*
+         * Final Variables
+         * when we dont want to change the existing values.
+         * 'final' keyword
+         */
+
+        // final int mynum = 15;
+        int mynum = 20; // will generate an error
+    }
+}
